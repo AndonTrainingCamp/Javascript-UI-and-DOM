1b2bf88c9a5571189de7a0b09f812d428e235a29
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-05-06 13:05:17 +0300

    Update Task-1.js

--- HTML-templates/Task-1.js
+++ HTML-templates/Task-1.js
@@ -19,24 +19,24 @@ let inputData = {
 };
 
 function setTemplate(el, context) {
-    Handlebars.registerHelper('table', function(items) {
-        let out = '<table class="items-table"><thead><tr><th>#';
-        items = context;
-        for (let el of items.headers) {
-            out += '<th>' + el + '</th>';
-        }
-        out += '</th></tr></thead>';
-        out += '<tbody>';
-        for (let index in items.items) {
-            out += '<tr><td>' + index + '</td>';
-            for (let value in items.items[index]) {
-                out += '<td>' + items.items[index][value] + '</td>';
-            }
-            out += '</tr>';
-        }
-        out += '</tbody></table>'
-        return new Handlebars.SafeString(out);
-    });
+//     Handlebars.registerHelper('table', function(items) {
+//         let out = '<table class="items-table"><thead><tr><th>#';
+//         items = context;
+//         for (let el of items.headers) {
+//             out += '<th>' + el + '</th>';
+//         }
+//         out += '</th></tr></thead>';
+//         out += '<tbody>';
+//         for (let index in items.items) {
+//             out += '<tr><td>' + index + '</td>';
+//             for (let value in items.items[index]) {
+//                 out += '<td>' + items.items[index][value] + '</td>';
+//             }
+//             out += '</tr>';
+//         }
+//         out += '</tbody></table>'
+//         return new Handlebars.SafeString(out);
+//     });
     let source = document.getElementById('entry-template').innerHTML;
     let template = Handlebars.compile(source);
     let html = template(context);
