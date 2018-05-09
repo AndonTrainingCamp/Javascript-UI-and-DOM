14c19d796cd0d3791f639106dcae1eeb50c381d0
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-05-09 16:25:19 +0300

    Update and rename Task-3.js to Task-2.js

---
+++ HTML5-Canvas/Task-2.js
@@ -0,0 +1,58 @@
+/*jslint devel: true */
+/*jslint es6 */
+'use strict';
+
+$(document).ready(main);
+
+function main() {
+    $('html, body').css({
+        'width': '100%',
+        'height': '100%',
+        'margin': '0px',
+        'overflow': 'hidden'
+    });
+    $('body').append('<canvas id="canvasSnake">');
+    $('#canvasSnake').css('display', 'block');
+    window.requestAnimationFrame(movingCircle);
+
+    let canvas = document.getElementById('canvasSnake');
+    let ctx = canvas.getContext('2d');
+
+    let ellipse = {
+        x: 40, // Initial values
+        y: 75,
+        r: 40,
+        drawEllipse: function () {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+            ctx.closePath();
+            ctx.fillStyle = 'rgb(123, 180, 224)';
+            ctx.fill();
+        }
+    };
+
+    let sx = 4;
+    let sy = 4;
+
+    function movingCircle() {
+        ctx.canvas.width = window.innerWidth;
+        ctx.canvas.height = window.innerHeight;
+        window.addEventListener('resize', function () {
+            ctx.canvas.width = window.innerWidth;
+            ctx.canvas.height = window.innerHeight;
+            if (ellipse.x > ctx.canvas.width) {
+                ellipse.x = ctx.canvas.width - ellipse.r - sx - 1;
+                ellipse.y = ctx.canvas.height - ellipse.r - sy - 1;
+            }
+        });
+        ellipse.drawEllipse();
+        ellipse.x += sx;
+        ellipse.y += sy;
+        if (ellipse.x >= ctx.canvas.width - ellipse.r || ellipse.x <= 0 + ellipse.r) {
+            sx *= -1;
+        } else if (ellipse.y >= ctx.canvas.height - ellipse.r || ellipse.y <= 0 + ellipse.r) {
+            sy *= -1;
+        }
+        window.requestAnimationFrame(movingCircle);
+    }
+}
