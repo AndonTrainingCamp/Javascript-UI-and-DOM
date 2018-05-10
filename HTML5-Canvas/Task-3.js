93476f6673982aad710e690903c9501cf551ee92
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-05-10 18:22:06 +0300

    Update Task-3.js

--- HTML5-Canvas/Task-3.js
+++ HTML5-Canvas/Task-3.js
@@ -25,21 +25,21 @@ function main() {
         this.y = Math.floor(Math.random() * ctx.canvas.height);
         this.a = a || 20;
         this.b = b || 20;
-        this.color = color || 'rgb(237, 126, 16)';
+        this.color = color || 'rgb(255, 79, 252)';
         this.drawRect = function () {
             ctx.fillStyle = this.color;
-            ctx.fillRect(this.x, this.y, this.a, this.b, );
+            ctx.fillRect(this.x, this.y, this.a, this.b);
         };
     }
 
     let head = new Rect();
     let snakeTail = [];
     let foods = [];
-    let obstacles = [];
 
     let sx = 0;
     let sy = 0;
     let speed = 1;
+    let direction = '';
 
     window.addEventListener('resize', function () {
         ctx.canvas.width = window.innerWidth;
@@ -56,22 +56,26 @@ function main() {
         if (event.key === 'w') {
             sy = -speed;
             sx = 0;
+            direction = 'up';
         }
         if (event.key === 's') {
             sy = speed;
             sx = 0;
+            direction = 'down';
         }
         if (event.key === 'a') {
             sx = -speed;
             sy = 0;
+            direction = 'left';
         }
         if (event.key === 'd') {
             sx = speed;
             sy = 0;
+            direction = 'right';
         }
     });
 
-    for (let i = 0; i < window.innerWidth * 6; i++) {
+    for (let i = 0; i < window.innerWidth / 10; i++) {
         let food = new Rect(10, 10, 'rgb(132, 201, 12)');
         foods.push(food);
     }
@@ -80,17 +84,37 @@ function main() {
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         foods.forEach(el => el.drawRect());
         head.drawRect();
-        
-        for (let i = 0; i < foods.length; i++) {
-            if (head.x <= foods[i].x + 9 && head.x >= foods[i].x - 19 &&
-                head.y <= foods[i].y + 9 && head.y >= foods[i].y - 19) {
-                    snakeTail.push(foods[i]);
-                    foods.splice(i, 1);
+        head.x += sx; // Head moving
+        head.y += sy;
+
+        for (let i = 0, j = 10; i < snakeTail.length; i++, j += 10) {
+            if (direction === 'up') {
+                snakeTail[i].x = head.x;
+                snakeTail[i].y = head.y + j;
+                snakeTail[i].drawRect();
+            } else if (direction === 'down') {
+                snakeTail[i].x = head.x;
+                snakeTail[i].y = head.y - j;
+                snakeTail[i].drawRect();
+            } else if (direction === 'left') {
+                snakeTail[i].x = head.x + j;
+                snakeTail[i].y = head.y;
+                snakeTail[i].drawRect();
+            } else if (direction === 'right') {
+                snakeTail[i].x = head.x - j;
+                snakeTail[i].y = head.y;
+                snakeTail[i].drawRect();
             }
         }
 
-        head.x += sx;  // Head moving
-        head.y += sy;
+        for (let i = 0; i < foods.length; i++) {
+            if (head.x <= foods[i].x + 9 && head.x >= foods[i].x - 19 &&
+                head.y <= foods[i].y + 9 && head.y >= foods[i].y - 19) {
+                let newPiece = new Rect(null, null, randomColor());
+                snakeTail.push(newPiece);
+                foods.splice(i, 1);
+            }
+        }
 
         if (head.x >= ctx.canvas.width - head.a || head.x <= 0) {
             sx *= -1;
@@ -99,4 +123,10 @@ function main() {
         }
         window.requestAnimationFrame(snakeAnimation);
     }
+}
+
+function randomColor() {
+    return 'rgb(' + Math.floor(Math.random() * 100 + 100) +
+        ', ' + Math.floor(Math.random() * 155) +
+        ', ' + Math.floor(Math.random() * 100 + 155) + ')';
 }
